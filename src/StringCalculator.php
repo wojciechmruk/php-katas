13135dcfd471783d0dba3fbcdc7597887d7b0919
Wojciech Mruk <vojciechmruk@gmail.com>
2017-08-27 08:36:49 +0100

    Continue - StringCalculatorSpec

--- src/StringCalculator.php
+++ src/StringCalculator.php
@@ -5,18 +5,9 @@ class StringCalculator
 
     public function add($numbers = '')
     {
-
-        if (empty($numbers)) {
-            return 0;
-        }
-
         $numbers = preg_split("/(,|\n)/", $numbers);
 
-        $result = 0;
-        foreach ($numbers as $number) {
-            $result += $number;
-        }
+        return array_sum($numbers);
 
-        return $result;
     }
 }
